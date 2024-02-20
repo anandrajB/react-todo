3db45f5fd5973dedf8af3272a005edb53f5f221c
anandrajB <anand98.ar@gmail.com>
2024-02-20 17:02:26 +0530

    updated button

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App
-      token="token 1fa40a2f9888cff8d85fa64ca75f52d36a192a4d"
+      token="token 28d832fb8fe5c4e3cec398a87af49b75971e405f"
       config_id="64ab94d14078dbac86787aed"
       base_url="finflo-test-v2-uikte.ondigitalocean.app"
       party_id="23"
